dcc9d92dfa39bf74939d3d1cde4188ce35fa1295
최서연 <yeondublue@naver.com>
2021-04-06 22:27:43 +0900

    add weather view

---
+++ WeatherApp/WeatherViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherViewController.swift
+//  WeatherApp
+//
+//  Created by yeonBlue on 2021/04/06.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    // MARK: - IBOutlet
+    @IBOutlet weak var weatherScrollView: UIScrollView!
+    @IBOutlet weak var weatherPageControl: UIPageControl!
+    @IBOutlet weak var addWeatherButton: UIBarButtonItem!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
